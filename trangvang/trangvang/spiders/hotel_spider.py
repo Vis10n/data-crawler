0c11ec59496d55dd91c15c2dcca3a087f7798180
Vis10n <zzchien96zz@gmail.com>
2019-06-04 11:16:05 +0700

    completed hotelCrawler

--- trangvang/trangvang/spiders/hotel_spider.py
+++ trangvang/trangvang/spiders/hotel_spider.py
@@ -1,26 +1,52 @@
 import scrapy
+import csv
+import re
 
-class QuotesSpider(scrapy.Spider):
-        name = "hotel"
-        start_url = [
-            'https://trangvangvietnam.com/categories/127160/khach-san.html'    
-        ]
+# File labels
+data_hotel_file = 'D:\Documents\Work\Data Crawler\\trangvang\\trangvang\spiders\data\hotel.csv'
 
-        def parse(self, response):
-            print("cmm Thanh!")
-            start_url = 'https://trangvangvietnam.com/categories/127160/khach-san.html'
-            finalPage = start_url + response.xpath('//div[@id="contentglobals"]/div[@id="main_seach"]/div[@id="main_seachl"]/div[@id="listingsearch"]/div[@id="paging"]/a/@href')[-2].extract()
-            totalPage = int(finalPage.split("=")[-1])
-            for page in range(int(totalPage)):
-                link = finalPage.replace(str(totalPage), str(page + 1))
-                print(link)
-                yield scrapy.Request(link, callback=self.crawlHotel)
+class HotelSpider(scrapy.Spider):
+    name = "hotelSpider"
+    start_urls = [
+        'https://trangvangvietnam.com/categories/127160/khach-san.html'    
+    ]
 
-        def crawlHotel(self, response):
-            for linkHotel in response.xpath('//div[@id="contentglobals"]/div[@id="main_seach"]/div[@id="main_seachl"]/div[@id="listingsearch"]/div[@class="boxlistings"]/div[@class="listings_top"]/div[@class="noidungchinh"]/h2[@class="company_name"]/a/@href').extract():
-                print(linkHotel)
-                yield scrapy.Request(linkHotel, callback=self.saveFile)
-        
-        def saveFile(self, response):
-            companyNameRaw = response.xpath('//div[@id="contentglobals"]/div[@id="listing_detail_left"]/div[@id="listing_basic_info"]/div[@class="tencongty"]/h1/text()').extract()
-            print(companyNameRaw)
+    def parse(self, response):
+        print("cmm Thanh!")
+
+        print("Prepare something...")
+        with open(data_hotel_file, 'a', newline='', encoding='utf8') as dataFile:
+            row = ['COMPANY_NAME', 'EMAIL_ADDRESS', 'TELE_NUMBERS']
+            writer = csv.writer(dataFile)
+            writer.writerow(row)
+        dataFile.close()
+
+        start_url = 'https://trangvangvietnam.com/categories/127160/khach-san.html'
+        finalPage = start_url + response.xpath('//div[@id="contentglobals"]/div[@id="main_seach"]/div[@id="main_seachl"]/div[@id="listingsearch"]/div[@id="paging"]/a/@href')[-2].extract()
+        totalPage = int(finalPage.split("=")[-1])
+
+        print("Begin the party!!")
+        for page in range(int(totalPage)):
+            link = finalPage.replace(str(totalPage), str(page + 1))
+            print(link)
+            yield scrapy.Request(link, callback=self.crawlHotel)
+
+    def crawlHotel(self, response):
+        for linkHotel in response.xpath(
+                '//div[@id="contentglobals"]/div[@id="main_seach"]/div[@id="main_seachl"]/div[@id="listingsearch"]/div[@class="boxlistings"]/div[@class="listings_top"]/div[@class="noidungchinh"]/h2[@class="company_name"]/a/@href'
+                ).extract():
+            yield scrapy.Request(linkHotel, callback=self.saveFile)
+    
+    def saveFile(self, response):
+        companyNameRaw = response.xpath('//div[@id="contentglobals"]/div[@id="listing_detail_left"]/div[@id="listing_basic_info"]/div[@class="tencongty"]/h1/text()').get()
+        emailRaw = response.xpath('//div[@id="contentglobals"]/div[@id="listing_detail_left"]/div[@id="listing_basic_info"]/div[@class="email_website"]/div[@class="box_email_website"]/div[@class="text_email"]/p/a/text()').get()
+        teleNumberRaw = response.xpath('//div[@id="contentglobals"]/div[@id="listing_detail_left"]/div[@id="listing_basic_info"]/div[@class="logo_diachi_xacthuc"]/div[@class="diachi_chitietcongty"]/div[@class="diachi_chitiet_li"]/div[@class="diachi_chitiet_li2"]/span/text()').get()
+        print(str(companyNameRaw))
+        companyName = str(companyNameRaw)
+        email = str(emailRaw)
+        teleNumber = str(re.search(r'[0-9\(\)\.\,\s]+', teleNumberRaw).group())
+        with open(data_hotel_file, 'a', newline='', encoding='utf8') as dataFile:
+            row = [companyName, email, teleNumber]
+            writer = csv.writer(dataFile)
+            writer.writerow(row)
+        dataFile.close()
--- trangvang/trangvang/spiders/test_spider.py
+++ trangvang/trangvang/spiders/test_spider.py
@@ -1,10 +1,13 @@
 import scrapy
+import pdb
+import re
+class NewsSpider(scrapy.Spider):
+  name = "tech-industry"
+  start_urls = [ 'https://trangvangvietnam.com/categories/127160/khach-san.html']
 
-class QuotesSpider(scrapy.Spider):
-    name='test'
-    start_url = [
-        'https://trangvangvietnam.com/categories/127160/khach-san.html'
-    ]
-
-    def parse(self, response):
-        print("cmm Thanh!")
+  def parse(self, response):
+    source = "https://www.cnet.com"
+    list_topic = response.xpath('//section[@id="topicListing"]/div')
+    links =  list_topic[1].xpath('//div[@class = "row asset"]/div[@class="col-2 assetThumb"]/a/@href').extract()
+    regex = r"\/news\/.*"
+    print("cmm")
